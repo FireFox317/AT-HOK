f6fab437d002ac6bf3330ca7b577a50cc7ff381b
IvoVarenhorst <i.a.m.varenhorst@student.utwente.nl>
2018-04-11 12:00:29 +0200

    kutchat.h

--- chat.h
+++ chat.h
@@ -2,26 +2,14 @@
  * chat.h
  *
  *  Created on: Apr 11, 2018
- *      Authors: Ivo & Niek
+ *      Author: ubuntu
  */
-
-#include <iostream>
-#include <string>
 
 #ifndef CHAT_H_
 #define CHAT_H_
 
-class chat
-{
-protected:
-    std::string sentMessage;
-    std::string receivedMessage;
+
+
     
-public:
-    chat();
-    virtual ~chat();
-    void setReceivedMessage(std::string message);
-    void printReceivedMessage();
-};
 
 #endif /* CHAT_H_ */
